936093a2aa14ab707b21ceb1ed876132fa22fc37
mridxl <mridxl03@gmail.com>
2025-02-20 16:15:50 +0530

    Add ui and some logic

---
+++ src/js/main.js
@@ -0,0 +1,151 @@
+const MAX_DISTANCE = 100000000;
+
+// LiftSystem class
+class LiftSystem {
+	constructor(floors, lifts) {
+		this.totalFloors = floors;
+		this.totalLifts = lifts;
+
+		this.liftState = new Array(lifts).fill().map(() => ({
+			currentFloor: 0,
+			requestQueue: [],
+		}));
+
+		this.pendingRequests = []; // global(kinda) request queue for requests that are not yet assigned to any lift
+	}
+
+	requestLift(floor) {
+		const liftToAssign = this.findClosestLift(floor);
+		if (liftToAssign === -1) {
+			if (!this.pendingRequests.includes(floor))
+				this.pendingRequests.push(floor);
+		} else {
+			this.liftState[liftToAssign].requestQueue.push(floor);
+			// TODO move lift to floor
+		}
+	}
+
+	findClosestLift(floor) {
+		let closestLift = -1;
+		let minDistance = MAX_DISTANCE;
+
+		for (let i = 0; i < this.totalFloors; i++) {
+			const distance = Math.abs(this.liftState[i].currentFloor - floor);
+			if (
+				distance < minDistance &&
+				this.liftState[i].requestQueue.length === 0
+			) {
+				minDistance = distance;
+				closestLift = i;
+			}
+		}
+		return closestLift; //returns index of lift closest of floor
+	}
+}
+
+// UI Logic
+document.addEventListener('DOMContentLoaded', () => {
+	const submitButton = document.getElementById('submit');
+	const liftInput = document.getElementById('lifts');
+	const floorInput = document.getElementById('floors');
+
+	submitButton.addEventListener('click', () => {
+		const lifts = parseInt(liftInput.value);
+		const floors = parseInt(floorInput.value);
+		if (isNaN(lifts) || isNaN(floors)) {
+			alert(
+				'Please enter the number of lifts and floors to proceed with the simulation'
+			);
+			return;
+		}
+		if (lifts > 10 || floors > 10 || lifts <= 0 || floors < 2) {
+			alert(
+				'Please enter a valid number of lifts less than 10 and floors less than 10 to proceed with the simulation'
+			);
+			return;
+		}
+
+		const liftSystem = new LiftSystem(floors, lifts);
+		console.log('Lift System Created', liftSystem);
+
+		document.getElementById('screen-one').remove();
+		document.getElementById('screen-two').style.display = 'flex';
+		document.getElementById('header-container').classList.add('moved');
+
+		generateFloors(floors);
+		generateLifts(lifts, floors);
+	});
+});
+
+function generateFloors(floors) {
+	const floorContainer = document.getElementById('floor-container');
+	for (let i = 0; i < floors; i++) {
+		const floorElement = document.createElement('div');
+		floorElement.classList.add('floor');
+		floorElement.style.height = `${100 / floors}%`;
+
+		const floorLabel = document.createElement('div');
+		floorLabel.classList.add('floor-label');
+		floorLabel.innerText = 'Floor ' + (floors - i);
+
+		floorElement.append(floorLabel);
+		floorContainer.append(floorElement);
+		generateButtons(floorElement);
+	}
+}
+
+function generateLifts(lifts, floors) {
+	const liftContainer = document.getElementById('lift-container');
+	for (let i = 0; i < lifts; i++) {
+		const liftElement = document.createElement('div');
+		liftElement.classList.add('lift');
+		liftElement.style.height = `${100 / (floors + 1)}%`;
+		liftElement.style.width = `${100 / (lifts + 5)}%`;
+		liftElement.style.left = `${((i + 0.2) * 100) / lifts}%`;
+		liftContainer.append(liftElement);
+		generateDoors(liftElement);
+	}
+}
+
+function generateButtons(floorElement) {
+	const btnContainer = document.createElement('div');
+	btnContainer.classList.add('button-container');
+
+	const upButton = document.createElement('button');
+	upButton.classList.add('up', 'lift-button');
+	upButton.innerText = '▲';
+	upButton.addEventListener('click', () => {
+		openDoors(document.querySelector('.lift'));
+	});
+
+	const downButton = document.createElement('button');
+	downButton.classList.add('down', 'lift-button');
+	downButton.innerText = '▼';
+
+	floorElement.append(btnContainer);
+	btnContainer.append(upButton, downButton);
+}
+
+function generateDoors(liftElement) {
+	const leftDoor = document.createElement('div');
+	leftDoor.classList.add('door', 'left');
+
+	const rightDoor = document.createElement('div');
+	rightDoor.classList.add('door', 'right');
+
+	liftElement.append(leftDoor, rightDoor);
+}
+
+function openDoors(liftElement) {
+	console.log('Opening');
+	const leftDoor = liftElement.querySelector('.left');
+	const rightDoor = liftElement.querySelector('.right');
+
+	leftDoor.classList.add('open');
+	rightDoor.classList.add('open');
+
+	setTimeout(() => {
+		leftDoor.classList.remove('open');
+		rightDoor.classList.remove('open');
+	}, 2500);
+}
